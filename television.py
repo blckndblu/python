e3be0d8128dfd3fb6be22c30e2180500281e9b4c
blckndblu <dawsonbrodine@gmail.com>
2025-11-21 14:40:47 -0600

    Update television.py

--- television.py
+++ television.py
@@ -1,7 +1,4 @@
 class Television:
-    """
-    A class representing the details of a television object.
-    """
     #Class Variables
     MIN_VOLUME = 0
     MAX_VOLUME = 2
@@ -9,13 +6,6 @@ class Television:
     MAX_CHANNEL = 3
 
     def __init__(self):
-        """
-        Method to set default values for a television object
-        :default status: False (TV Off)
-        :default muted: False (Not muted)
-        :default volume: MIN_VOLUME = 0
-        :default channel: MIN_CHANNEL = 0
-        """
         self._status = False
         self._muted = False
         self._volume = self.MIN_VOLUME
@@ -25,18 +15,12 @@ class Television:
         return f"Power = {self._status}, Channel = {self._channel}, Volume = {self._volume}"
 
     def power(self):
-        """
-        Method to change power status of television object
-        """
         if self._status:
             self._status = False
         else:
             self._status = True
 
     def mute(self):
-        """
-        Method to change muted status of television object if television status is True
-        """
         if self._status:
             if self._muted:
                 self._muted = False
@@ -46,10 +30,6 @@ class Television:
             pass
 
     def channel_up(self):
-        """
-        Method to increase channel value of television object if television status is True
-        If channel is at MAX_CHANNEL, then channel becomes MIN_CHANNEL
-        """
         if self._status:
             if self._channel == self.MAX_CHANNEL:
                 self._channel = self.MIN_CHANNEL
@@ -59,10 +39,6 @@ class Television:
             pass
 
     def channel_down(self):
-        """
-        Method to decrease channel value of television object if television status is True
-        If channel is at MIN_CHANNEL, then channel becomes MAX_CHANNEL
-        """
         if self._status:
             if self._channel == self.MIN_CHANNEL:
                 self._channel = self.MAX_CHANNEL
@@ -72,10 +48,6 @@ class Television:
             pass
 
     def volume_up(self):
-        """
-        Method to increase volume value of television object if television status is True
-        If volume is at MAX_VOLUME, then volume is unchanged
-        """
         if self._status:
             if self._volume != self.MAX_VOLUME:
                 self._volume += 1
@@ -85,10 +57,6 @@ class Television:
             pass
 
     def volume_down(self):
-        """
-        Method to decrease volume value of television object if television status is True
-        If volume is at MIN_VOLUME, then volume is unchanged
-        """
         if self._status:
             if self._volume != self.MIN_VOLUME:
                 self._volume -= 1
@@ -96,3 +64,4 @@ class Television:
                 pass
         else:
             pass
+
